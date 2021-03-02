7588b8364cccbcde3423425f05f3ef8ad3b5eb80
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-02 10:54:24 -0500

    re-implement worker selection
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/db/dbtest/builder.go
+++ atc/db/dbtest/builder.go
@@ -27,7 +27,8 @@ func BaseWorker(name string) atc.Worker {
 	return atc.Worker{
 		Name: name,
 
-		Version: concourse.WorkerVersion,
+		Platform: "linux",
+		Version:  concourse.WorkerVersion,
 
 		GardenAddr:      unique("garden-addr"),
 		BaggageclaimURL: unique("baggageclaim-url"),
